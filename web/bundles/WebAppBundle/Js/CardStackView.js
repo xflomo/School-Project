6ac48de3c3a2949372dc9d5cde65cd421c414762
Florian Breuer <f.breuer@a3plus.de>
2017-11-22 20:08:40 +0100

    integration card flip

---
+++ web/bundles/WebAppBundle/Js/CardStackView.js
@@ -0,0 +1,79 @@
+var owl = $('.owl-carousel');
+
+$(window).load(function () {
+    $(".flip-card-effect").flip({
+        trigger: 'manual',
+    });
+
+    var options={
+        margin: 0,
+        nav: true,
+        items: 1,
+        animateOut: 'slideOutDown',
+        animateIn: 'fadeIn',
+        onInitialize: initCardCounter,
+        onChanged: changeCard
+    };
+    owl.owlCarousel(options);
+
+    $('#karteikarte .karteikart-content.front').textfill({
+        minFontPixels: 22,
+    });
+
+    $('#karteikarte .karteikart-content.back').textfill({
+        minFontPixels: 22,
+    });
+
+
+});
+
+$('.turn-card-action-btn').on('click', function () {
+    turnCardAndCheckUserInput();
+});
+
+$('.turn-card-back-action-btn').on('click', function () {
+    turnCardBack();
+});
+
+
+
+/**********************************
+ *
+ * Funktionen
+ *
+ **********************************/
+
+function initCardCounter(event) {
+    var items  = event.item.count;
+    $('.anzahl-karten span').text("Karte 1 von "+items);
+}
+
+function changeCard(e) {
+    // refresh counter
+    var currentCard = e.item.index + 1;
+    var maxCards = e.item.count;
+    $('.anzahl-karten span').text("Karte "+currentCard+" von "+maxCards);
+
+    // refresh card Flip
+    refreshCardFlips();
+}
+
+function refreshCardFlips() {
+    setTimeout(function(){
+        $(".flip-card-effect").flip(false);
+    }, 800);
+    $('.btn-enter.turn-card-back-action-btn').hide();
+    $('.btn-enter.turn-card-action-btn').show();
+}
+
+function turnCardAndCheckUserInput() {
+    $(".owl-item.active .flip-card-effect").flip(true);
+    $('.btn-enter.turn-card-back-action-btn').show();
+    $('.btn-enter.turn-card-action-btn').hide();
+}
+
+function turnCardBack() {
+    $(".owl-item.active .flip-card-effect").flip(false);
+    $('.btn-enter.turn-card-back-action-btn').hide();
+    $('.btn-enter.turn-card-action-btn').show();
+}
